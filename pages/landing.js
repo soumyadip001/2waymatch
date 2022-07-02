71e30bcb9a85e353eff2732e40849614972fa7ec
Soumyadip <soumyadiphazra@gmail.com>
2022-07-02 11:41:24 +0530

    updated linter config

--- pages/landing.js
+++ pages/landing.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 // components
 
@@ -7,27 +6,27 @@ import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
 export default function Landing() {
-    return (
-        <>
-            <Navbar transparent />
-            <main>
-                <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
-                    <div
-                        className="absolute top-0 w-full h-full bg-center bg-cover"
-                        style={{
-                            backgroundImage:
-                                "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
-                            }}
-                        >
-                        <span
-                            id="blackOverlay"
-                            className="w-full h-full absolute opacity-75 bg-black"
-                        ></span>
-                    </div>
-                    {/* start here */}
-                </div>
-            </main>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Navbar transparent />
+      <main>
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+          <div
+            className="absolute top-0 w-full h-full bg-center bg-cover"
+            style={{
+                backgroundImage:
+                    "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+            }}
+          >
+            <span
+              id="blackOverlay"
+              className="w-full h-full absolute opacity-75 bg-black"
+            ></span>
+          </div>
+          {/* start here */}
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
 }
